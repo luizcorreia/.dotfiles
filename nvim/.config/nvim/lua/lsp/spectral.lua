f544025ec974917fe9b8f75487d99c073ec3e745
Luiz Correia <luiz@luizcorreia.eti.br>
2021-11-19 19:27:25 -0300

    feat: add spectral language server

--- nvim/.config/nvim/lua/lsp/spectral.lua
+++ nvim/.config/nvim/lua/lsp/spectral.lua
@@ -1,15 +1,50 @@
 local lspconfig = require 'lspconfig'
+local configs = require 'lspconfig/configs'
+local util = require 'lspconfig/util'
+
+local server_name = 'spectral'
+local bin_name = 'spectral-language-server'
+
+-- Check if the config is already defined (useful when reloading this file)
+if not lspconfig[server_name] then
+  configs[server_name] = {
+    default_config = {
+      cmd = { bin_name, '--stdio' },
+      filetypes = { 'yaml', 'json', 'yml' },
+      root_dir = util.root_pattern('.spectral.yaml', '.spectral.yml'),
+      single_file_support = true,
+      settings = {
+        enable = true,
+        run = 'onType',
+        validateLanguages = { 'yaml', 'json', 'yml' },
+      },
+    },
+    docs = {
+      description = [[
+          https://github.com/luizcorreia/spectral-language-server
+           `A flexible JSON/YAML linter for creating automated style guides, with baked in support for OpenAPI v2 & v3.`
+
+          `spectral-language-server` can be installed via `npm`:
+          ```sh
+          npm i -g spectral-language-server
+          ```
+        See [vscode-spectral](https://github.com/stoplightio/vscode-spectral#extension-settings) for configuration options.
+        ]],
+    },
+  }
+end
 
 local M = {}
 local settings = {
   filetypes = {
     'json',
     'yaml',
+    'yml',
   },
 }
 M.setup = function(on_attach, capabilities)
   lspconfig.spectral.setup {
-    cmd = { 'spectral lint %' },
+    -- cmd = { 'spectral lint %' },
     filetypes = settings.filetypes,
     on_attach = on_attach,
     capabilities = capabilities,
